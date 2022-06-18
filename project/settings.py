752950bbef7de6b840b6527753dea2ff7677bc37
Alihassan20 <43120067+Alihassan20@users.noreply.github.com>
2022-06-18 16:21:08 +0400

    add form job

--- project/settings.py
+++ project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'job',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -119,10 +120,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
-]
+
+#STATICFILES_DIRS = [
+ #   os.path.join(BASE_DIR, "static"),
+  #  '/var/www/static/',
+#]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR, "media")
