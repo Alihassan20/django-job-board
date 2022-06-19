697d9efb519ca82f8742feebd821ae7c78ecc40f
Alihassan20 <43120067+Alihassan20@users.noreply.github.com>
2022-06-19 18:33:03 +0400

    add EDIT PROFILE

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
+from django.urls import reverse
 from django.contrib.auth import authenticate ,login
 from django.shortcuts import render
-from .forms import SignupForm
+from .forms import SignupForm ,UserForm ,ProfileForm
 from django.shortcuts import redirect,render
 from .models import Profile
 
@@ -31,4 +32,31 @@ def profile(request):
 
 
 def profile_edit(request):
-    return render(request,'accounts/profile_edit.html',{})
+    profile = Profile.objects.get(user = request.user)  
+      
+    if request.method=='POST':
+        userform =UserForm(request.POST,instance=request.user)
+        profileform= ProfileForm(request.POST,request.FILES,instance=profile)
+        if userform.is_valid() and profileform.is_valid():
+            userform.save()
+            myprofile=profileform.save(commit=False)
+            myprofile.user = request.user
+            myprofile.save()
+            return redirect(reverse('accounts:profile'))
+
+    else:
+        userform =UserForm(instance=request.user)
+        profileform= ProfileForm(instance=profile)
+        
+    
+    return render(request,'accounts/profile_edit.html',{'userform':userform,'profileform':profileform})
+
+
+
+
+
+
+
+
+
+
